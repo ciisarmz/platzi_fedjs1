905c98e7d1204c73257a3fcff81772f3e48fd47d
César Zubiate <ciisarmz@gmail.com>
2023-03-16 11:04:41 -0500

    menu mobile agregado

--- main.js
+++ main.js
@@ -1,8 +1,12 @@
 const menuEmail = document.querySelector('.navbar-email');
 const eMenu = document.querySelector('.desktop-menu');
+const mobileMenuIcon = document.querySelector('.menuIcon');
+const mobileMenu = document.querySelector('.mobile-menu');
 
 menuEmail.addEventListener('click', toggleMenu);
+mobileMenuIcon.addEventListener('click', toggleMenu);
 
 function toggleMenu() {
     eMenu.classList.toggle('inactive');
+    mobileMenu.classList.toggle('inactive');
 }
